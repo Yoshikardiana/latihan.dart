11c5623f2129c513b81bf1948028f8da64bd0422
Yoshikardiana <110141459+Yoshikardiana@users.noreply.github.com>
2023-03-02 20:10:40 +0700

    Add files via upload

--- latihan1.dart
+++ latihan1.dart
@@ -1,7 +1,153 @@
-import 'dart:io';
+// void main() {
+//   for (int i = 5; i >= 1; i--) {
+//     print('hello ${i - 0 }');
+//   }
+// }
 
-  main() {
-    stdout.write('Siapa kamu');
-    var nama = stdin.readLineSync();
-    print("Hello $nama!");
-}
+// void main() {
+//   for (int i = 1; i <= 20; i++) {
+//     if (i % 2 != 0 ) {
+//         print(i);
+//     }
+//   }
+// }
+
+// void main() {
+//     for (int i = 1; i <= 20; i+= 2) {
+//         print(i);
+//     }
+// }
+
+// void main() {
+//     int i = 10;
+//     String namasaya = "yoshi";
+//     String alamat = "Tambakan jalancagak subang";
+//     String namasekolah = "SMKN 1 SUBANG";
+//     String citacita = "arsitek"; 
+
+//     print("nama saya $namasaya alamat $alamat saya bersekolah di $namasekolah cita-cita saya $citacita " );
+//   }
+
+// import 'dart:io';
+
+// main() {
+//     stdout.write("siapa kamu: ");
+//     var nama = stdin.readLineSync();
+//     print("Hello $nama!");
+// }
+
+// void main() {
+//     int nilai = 110;
+//     String nama = "yoshi";
+//     print("nama : $nama");
+//     print("nilai : $nilai");
+
+//     if( nilai > 100){
+//         print("predikat : INPUT MELEBIHI BATAS");
+//     }
+// }
+// main() {
+//     int total = 1000000;
+//     if(100001 > 100000){
+//       print("Selamat anda dapat hadiah!");
+//     }else if (total == 100000){
+//       print("Dapet diskon sedikit");
+//     }else{
+//     print("Ga dapet diskon sedikit");
+//     }
+//   }
+//   void main() {
+//     int i = 1000000;
+//     String nama = 'Yoshi kardiana';
+//     String alamat = 'kp.Babakan menteng Ds. Tambakan Kec. Jalancagak Kab. Subang Prov. Jawa barat Neg. Indonesia.';
+//     String sekolah = 'SMKN 1 SUBANG';
+//     String citacita = 'Pramugari++';
+   
+    
+
+//     print("Nama Saya $nama alamat $alamat saya bersekolah di $sekolah cia cita saya $citacita ");
+// }
+// void main() {
+//   String nama = "Dava";
+//   print ("Nama Lengkap : $nama");
+//   int total = 70;
+//   print ("Nilai $total ");
+//   if ( total > 100){
+//     print("Melebihi batas");
+//   }else if(total >= 90){
+//     print ("Predikat : A");
+//   }else if (total >= 80){
+//     print ("Predikat : B");
+//   }else if (total >= 70){
+//     print ("Predikat : C");
+//   }else{
+//     print ("Predikat : REMEDIAL");
+//   }
+  
+  
+// }
+// void main (){
+//   int a = 10;
+//   int b = 10;
+//   double hasil;
+  
+//   // hasil = a + b;
+//   // hasil = a - b;
+//   // hasil = a * b;
+//   hasil = a / b;
+  
+//   print("Hasil dari penjumlahan $a + $b = $hasil");
+//   print("Hasil dari pengurangan  $a - $b = $hasil");
+//  print("Hasil dari perkalian $a * $b = $hasil");
+ // print("Hasil dari pembagian $a / $b = $hasil");
+//}
+// import 'dart:io';
+
+// void main() {
+//   stdout.write('Masukkan Nilai A: ');
+//   var inputA = stdin.readLineSync();
+//   stdout.write('Masukkan Nilai B: ');
+//   var inputB = stdin.readLineSync();
+
+//   var a = int.parse(inputA!); // ubah inputA dari tipe String menjadi tipe integer
+//   var b = int.parse(inputB!); // ubah inputB dari tipe String menjadi tipe integer
+
+//   var hasilTambah = a + b;
+//   var hasilKurang = a - b;
+//   var hasilKali = a * b;
+//   var hasilBagi = a / b;
+
+//   print('Nilai A yang dimasukkan adalah: $a');
+//   print('Nilai B yang dimasukkan adalah: $b');
+//   print('Hasil Penjumlahan dari $a + $b adalah: $hasilTambah');
+//   print('Hasil Pengurangan dari $a - $b adalah: $hasilKurang');
+//   print('Hasil Perkalian dari $a * $b adalah: $hasilKali');
+//   print('Hasil Pembagian dari $a / $b adalah: $hasilBagi');
+// }
+
+// import 'dart:io';
+// void main () {
+//   print("LOGIN DZIKRI LINK");
+//   stdout.write('Masukan Nama : ');
+//   var nama = stdin.readLineSync();
+//   stdout.write('Password : ');
+//   var password = stdin.readLineSync();
+
+//     if(password == "12345" && nama == "DZ"){
+//     print("Selamat $nama Berhasil Login");
+//   } else {
+//     print("Maaf $nama Gagal Login");
+//   } 
+// }
+
+// void main(){
+//   var myList = ['yoshi', '11RPL2', 'islam', 'laki-laki', 'bidan', 'tambakan', 'plajar'];
+  
+//   print("Nama          : ${myList[0]}");
+//   print("Kelas         : ${myList[1]}");
+//   print("Agama         : ${myList[2]}");
+//   print("Jenis Kelamin : ${myList[3]}");
+//   print("Cita ciata    : ${myList[4]}");
+//   print("alamat        : ${myList[5]}");
+//   print("Status        : ${myList[6]}");
+// }
